ef39acc6f76f51f4e2c61b341a8cc9a65ab81fc3
Jason Azevedo <jasonazevedo03@gmail.com>
2023-11-18 21:13:54 +0200

    Setup the FSort program

---
+++ src/FSort/src/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << "Welcome to the FSort program!" << std::endl;
+
+    // TODO: Load the config
+    // TODO: YAML or JSON for config?
+
+    // TODO: Utils for logging (console and log file)
+
+    // TODO: Sort the files based on the config using another utils file
+
+    // TODO: Done!
+    return 0;
+}
